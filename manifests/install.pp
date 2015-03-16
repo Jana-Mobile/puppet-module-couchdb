4b080c6eac8183fc1fb4afa7a2f678ce8de2e350
Tom McLaughlin <tom@jana.com>
2015-03-16 18:05:01 -0400

    first pass at handling prefix differences between source and binary installs.

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,13 +4,21 @@ class couchdb::install {
     $version = '1.2'
   }
 
+  if $::couchdb::use_package == true {
+    $couch_prefix = ''
+    $logrotate_files = ["/etc/logrotate.d/couchdb"]
+  } else {
+    $couch_prefix = '/usr/local'
+    $logrotate_files = ["${couch_prefix}/etc/logrotate.d/couchdb", "/etc/logrotate.d/couchdb"]
+  }
+
   Exec {
-    unless => '/usr/bin/test -f /usr/local/bin/couchdb',
+    unless => "/usr/bin/test -f ${couch_prefix}/bin/couchdb",
   }
 
   user { 'couchdb':
     ensure      => present,
-    home        => '/usr/local/var/lib/couchdb',
+    home        => "${couch_prefix}/var/lib/couchdb",
     managehome  => false,
     comment     => 'CouchDB Administrator',
     shell       => '/bin/bash'
@@ -26,24 +34,27 @@ class couchdb::install {
   }
 
   file {
-    [$couchdb::database_dir, '/usr/local/etc/couchdb',
-    '/usr/local/var/log/couchdb', '/usr/local/var/run/couchdb']:
+    [$couchdb::database_dir, "${couch_prefix}/etc/couchdb",
+    "${couch_prefix}/var/log/couchdb", "${couch_prefix}/var/run/couchdb"]:
     ensure  => directory,
   }
 
-  file { '/usr/local/etc/couchdb/local.ini':
+  file { "${couch_prefix}/etc/couchdb/local.ini":
     ensure  => file,
     mode    => '0600',
     content => template('couchdb/usr/local/etc/couchdb/local.ini.erb'),
     notify  => Service['couchdb'];
   }
 
-  file { '/etc/init.d/couchdb':
-    ensure  => link,
-    target  => '/usr/local/etc/init.d/couchdb',
+  if $::couch::use_package == false {
+    file { '/etc/init.d/couchdb':
+      ensure  => link,
+      target  => "${couch_prefix}/etc/init.d/couchdb",
+    }
   }
 
-  file { ['/usr/local/etc/logrotate.d/couchdb', '/etc/logrotate.d/couchdb']:
+
+  file { $logrotate_files:
     ensure  => file,
     owner   => 'root',
     group   => 'root',
